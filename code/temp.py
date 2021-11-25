7215a6eece5949163623e122cd48e3bdfbd0b26e
Anton Gashi <anton@pop-os.localdomain>
2021-11-25 18:44:50 +0000

    started the proper triangle fitting method

--- code/temp.py
+++ code/temp.py
@@ -1,3 +1,4 @@
+from matplotlib import widgets
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as sco
@@ -6,39 +7,37 @@ import tifffile as tf
 image=0
 file=tf.TiffSequence("Perfect Spots r8.00/spot014.tif").asarray()
 
-def triangle3(x,max_,b):
-    fit=np.zeros_like(x)
-    for i in range(len(x)):
-        if np.absolute(x[i])>=1:
-            fit[i]=0
-        else:
-            fit[i]=1-np.absolute(x[i])
-    return (fit-b)*max_
+box_size=11
 
-def triangle4(x,cen,max_,b1,b2):
-    fit=np.zeros_like(x)
-    inc1_const=max_/(cen-b1)
-    inc2_const=max_/(b2-cen)
-    inc1,inc2=inc1_const,max_
-    for i in range(len(fit)):
-        if cen>=i>=b1:
-            fit[i]=inc1
-            inc1+=inc1_const
-        elif b2>=i>cen:
-            fit[i]=inc2
-            inc2-=inc2_const
+def triangle(centre,half_base,height,size):
+    x=np.linspace((centre-half_base),(centre),size)
+    y=np.linspace(0,height,size)
+    return x,y
 
-    return fit
+def tri_area(x,y):
+    area=np.zeros(len(x)-1)
+    base=np.absolute(x[0]-x[1])
+    for i in range(len(x)-1):
+        box_heigth=y[i]
+        box_area=base*box_heigth
+        tri_area=0.5*base*(y[i+1]-y[i])
+        area[i]=box_area+tri_area
+    return area
 
 
-x2=np.linspace(0,51,50)
-ym=triangle4(x2,23,55000,15,35)
-print(ym)
+tri_x,tri_y=triangle(24,7,60000,box_size)
+total_image_area=np.sum(file[image,24,20:30])
+tri_area=tri_area(tri_x,tri_y)
+res=np.absolute(tri_area-file[image,24,20:30])
 
-#popt2, pcov2 = sco.curve_fit(triangle4, x2,file[image,24,:],method='lm')
-#print(popt2)
-#ym=triangle4(x2,popt2[0],popt2[1],popt2[2],popt2[3])
 
-plt.plot(x2,ym)
-plt.plot(file[image,24,:],':')
+fig, (ax1,ax2) = plt.subplots(nrows=2,ncols=1,figsize=(8,4),constrained_layout=True)
+
+ax1.plot(tri_x,tri_y)
+ax1.plot(file[image,24,:])
+ax1.set_xlim(0,50)
+
+x=np.arange(20,30,1)
+ax2.bar(x,res)
+ax2.set_xlim(0,50)
 plt.show()
