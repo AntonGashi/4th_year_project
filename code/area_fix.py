b3f1e6b09418c573e10b847bded7abcd61b90747
AntonGashi <antongashi12345@gmail.com>
2022-03-11 19:34:32 +0000

    fridays work

--- code/area_fix.py
+++ code/area_fix.py
@@ -6,8 +6,9 @@ import math
 from numba import jit,njit
 import time
 from scipy.spatial import distance
+from matplotlib.ticker import (MultipleLocator,FormatStrFormatter, AutoMinorLocator)
 
-file_=tf.TiffSequence("Perfect Spots r1.00/Perfect Spots r1.00.tif").asarray().reshape(100,50,50)
+file_=tf.TiffSequence("Perfect Spots r8.00/Perfect Spots r8.00.tif").asarray().reshape(100,50,50)
 #file_=tf.TiffSequence("Noisy Spots/Noisy Spots r8.00/Noisy Spots r8.00.tif").asarray().reshape(100,50,50)
 groundtruth=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r1.00/groundtruth.csv"))+25
 
@@ -29,12 +30,11 @@ def triangle(centre,halfbase,height):
 def area(tri_in,file):
     grad=(tri_in[1][1]-tri_in[1][0])/(tri_in[0][1]-tri_in[0][0])
     start,stop=math.floor(tri_in[0][0]),math.ceil(tri_in[0][2])
-    area=np.zeros_like(file,dtype=float)#need to impliment a lower bound on the noise
+    area=np.zeros_like(file[0],dtype=float)#need to impliment a lower bound on the noise
     for i in range(start,stop):
         if i==start:
             height=((i+1)-tri_in[0][0])*grad
             area[i]=0.5*((i+1)-tri_in[0][0])*(height)
-            print(0.5*((i+1)-tri_in[0][0])*(height))
         elif i<math.floor(tri_in[0][1]):
             area[i]=height+(0.5*grad)
             height=height+grad
@@ -50,13 +50,38 @@ def area(tri_in,file):
             area[i-1]=0.5*((i-1)-tri_in[0][2])*height
     return area
 
-tri_var=triangle(24,5,100)
-tri_var_2=triangle(24.4,5,100)
-area_var=area(tri_var,file_sum_x)
-area_var_2=area(tri_var_2,file_sum_x)
+def res(file_in,area_,tri_in,image):
+    res=np.sum(file_in[image])-np.sum(area_)
+    display=abs(file_in[image]-area_)
+    #res=file_in[image]-area_
+    #### write about how changing this helped
+    return res**2,display
 
-#print(area_var)
-#print(area_var_2)
-#plt.plot(area_var)
-#plt.plot(area_var_2)
-#plt.show()
+
+tri_var=triangle(25,5,5e5)
+
+print(groundtruth[0])
+
+area_var=area(tri_var,file_sum_x)
+x=np.linspace(0,50,50)
+
+res_var=abs(file_sum_x[0]-area_var)
+
+fig, (ax1,ax2)=plt.subplots(2,1,sharex=True,gridspec_kw={'hspace': 0, 'wspace': 0})
+
+fig.supxlabel('Pixels (summed x-axis)')
+fig.supylabel('Pixel Intensities')
+ax1.plot(tri_var[0],tri_var[1],color='r',label='Triangle Function')
+ax1.bar(x,file_sum_x[0],label='Input Spot')
+#ax1.bar(x,area_var,label='Calculated Area')
+ax1.yaxis.set_major_formatter(FormatStrFormatter('% .0e'))
+ax1.legend()
+
+#ax2.bar(x,res_var,color='k',label='Residual Area')
+ax2.set_ylim(0,np.max(area_var))
+ax2.invert_yaxis()
+ax2.yaxis.set_major_formatter(FormatStrFormatter('% .0e'))
+
+plt.legend()
+plt.tight_layout()
+plt.savefig('visual_test.png',dpi=400)
--- code/new.py
+++ code/new.py
@@ -8,19 +8,15 @@ import time
 from scipy.spatial import distance
 
 
-file_=tf.TiffSequence("Perfect Spots r1.00/Perfect Spots r1.00.tif").asarray().reshape(100,50,50)
-#file_=tf.TiffSequence("Noisy Spots/Noisy Spots r8.00/Noisy Spots r8.00.tif").asarray().reshape(100,50,50)
-groundtruth=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r1.00/groundtruth.csv"))+25
 
-def sum_(file):
+
+def sum_(file_):
     file_x=np.zeros((100,50))
     file_y=np.zeros((100,50))
     for i in range(file_.shape[0]):
-        file_x[i]=np.sum(file[i],axis=0)
-        file_y[i]=np.sum(file[i],axis=1)
+        file_x[i]=np.sum(file_[i],axis=0)
+        file_y[i]=np.sum(file_[i],axis=1)
     return file_x,file_y
-
-file_sum_x,file_sum_y=sum_(file_)
 
 
 def triangle(centre,halfbase,height):
@@ -106,7 +102,7 @@ def search2(file_in,image):
     first_search=np.zeros((N,N,N))
     first_cen_guess=np.linspace(23, 27,N)
     first_hb_guess=np.linspace(3,8,N)
-    first_height_guess=np.linspace(1e5,6e5,N)
+    first_height_guess=np.linspace(1e5,7e5,N)
     for i in range(N):
         for j in range(N):
             for k in range(N):
@@ -132,11 +128,11 @@ def search2(file_in,image):
 
 def search3(file_in,image):
     global sweep_res_array
-    N=10
-    M=10
+    N=100
+    M=5
     sweep_res_array=np.zeros((N,M))
-    sweep_cen_values=np.linspace(23,27,N)
-    sweep_hb_values=np.linspace(0.5,5,M)
+    sweep_cen_values=np.linspace(22,28,N)
+    sweep_hb_values=np.linspace(2,8,M)
     max=7e5
     for i in range(N):
         for j in range(M):
@@ -151,7 +147,7 @@ def search4(file_in,image):
     center_values=np.linspace(22,28,N)
     cen_sweep=np.zeros(N)
     for i in range(N):
-        tri_var=triangle(center_values[i],3,6e5)
+        tri_var=triangle(center_values[i],4,6e5)
         area_var=area(tri_var,file_in)
         cen_sweep[i]=res(file_in,area_var,tri_var,image)
     result=np.where(np.min(cen_sweep)==cen_sweep)
@@ -171,75 +167,268 @@ def full_search(file_in_x,file_in_y):
             array_of_result_y[i]=np.average(result_y)
     return array_of_result_x,array_of_result_y
 
+### file variable define ###
+
+
+file_r1=tf.TiffSequence("Perfect Spots r1.00/Perfect Spots r1.00.tif").asarray().reshape(100,50,50)
+groundtruth_r1=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r1.00/groundtruth.csv"))+25
+file_sum_x_r1,file_sum_y_r1=sum_(file_r1)
+
+file_r141=tf.TiffSequence("Perfect Spots r1.41/Perfect Spots r1.41.tif").asarray().reshape(100,50,50)
+groundtruth_r141=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r1.41/groundtruth.csv"))+25
+file_sum_x_r141,file_sum_y_r141=sum_(file_r141)
+
+file_r2=tf.TiffSequence("Perfect Spots r2.00/Perfect Spots r2.00.tif").asarray().reshape(100,50,50)
+groundtruth_r2=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r2.00/groundtruth.csv"))+25
+file_r2_noise=tf.TiffSequence("Noisy Spots/Noisy Spots r2.00/Noisy Spots r2.00.tif").asarray().reshape(100,50,50)
+file_sum_x_r2,file_sum_y_r2=sum_(file_r2)
+file_sum_x_r2_noise,file_sum_y_r2_noise=sum_(file_r2_noise)
+
+
+file_r283=tf.TiffSequence("Perfect Spots r2.83/Perfect Spots r2.83.tif").asarray().reshape(100,50,50)
+groundtruth_r283=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r2.83/groundtruth.csv"))+25
+file_r283_noise=tf.TiffSequence("Noisy Spots/Noisy Spots r2.83/Noisy Spots r2.83.tif").asarray().reshape(100,50,50)
+file_sum_x_r283,file_sum_y_r283=sum_(file_r283)
+file_sum_x_r283_noise,file_sum_y_r283_noise=sum_(file_r283_noise)
+
+
+file_r4=tf.TiffSequence("Perfect Spots r4.00/Perfect Spots r4.00.tif").asarray().reshape(100,50,50)
+groundtruth_r4=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r4.00/groundtruth.csv"))+25
+file_r4_noise=tf.TiffSequence("Noisy Spots/Noisy Spots r4.00/Noisy Spots r4.00.tif").asarray().reshape(100,50,50)
+file_sum_x_r4,file_sum_y_r4=sum_(file_r4)
+file_sum_x_r4_noise,file_sum_y_r4_noise=sum_(file_r4_noise)
+
+
+file_r566=tf.TiffSequence("Perfect Spots r5.66/Perfect Spots r5.66.tif").asarray().reshape(100,50,50)
+groundtruth_r566=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r5.66/groundtruth.csv"))+25
+file_r566_noise=tf.TiffSequence("Noisy Spots/Noisy Spots r5.66/Noisy Spots r5.66.tif").asarray().reshape(100,50,50)
+file_sum_x_r566,file_sum_y_r566=sum_(file_r566)
+file_sum_x_r566_noise,file_sum_y_r566_noise=sum_(file_r566_noise)
+
+
+file_r8=tf.TiffSequence("Perfect Spots r8.00/Perfect Spots r8.00.tif").asarray().reshape(100,50,50)
+groundtruth_r8=pd.DataFrame.to_numpy(pd.read_csv("Perfect Spots r8.00/groundtruth.csv"))+25
+file_r8_noise=tf.TiffSequence("Noisy Spots/Noisy Spots r8.00/Noisy Spots r8.00.tif").asarray().reshape(100,50,50)
+file_sum_x_r8,file_sum_y_r8=sum_(file_r8)
+file_sum_x_r8_noise,file_sum_y_r8_noise=sum_(file_r8_noise)
+
+### file variable define end ###
+
 start=time.perf_counter()
-full_search_result_x,full_search_result_y=full_search(file_sum_x,file_sum_y)
+full_search_result_x_r1,full_search_result_y_r1=full_search(file_sum_x_r1,file_sum_y_r1)
+full_search_result_x_r141,full_search_result_y_r141=full_search(file_sum_x_r141,file_sum_y_r141)
+full_search_result_x_r2,full_search_result_y_r2=full_search(file_sum_x_r2,file_sum_y_r2)
+full_search_result_x_r283,full_search_result_y_r283=full_search(file_sum_x_r283,file_sum_y_r283)
+full_search_result_x_r4,full_search_result_y_r4=full_search(file_sum_x_r4,file_sum_y_r4)
+full_search_result_x_r566,full_search_result_y_r566=full_search(file_sum_x_r566,file_sum_y_r566)
+full_search_result_x_r8,full_search_result_y_r8=full_search(file_sum_x_r8,file_sum_y_r8)
 stop=time.perf_counter()
 
 timetaken=stop-start
 
-
 ###
 #All for display#
 ###
-error_x=abs(full_search_result_x-(groundtruth[:,1]))
-error_y=abs(full_search_result_y-(groundtruth[:,0]))
+error_x_r1=abs(full_search_result_x_r1-(groundtruth_r1[:,1]))
+error_y_r1=abs(full_search_result_y_r1-(groundtruth_r1[:,0]))
 
-calculated_points=np.stack((full_search_result_x,full_search_result_y),axis=1)
-sub_of_cal_ground=groundtruth.reshape(2,100)-calculated_points.reshape(2,100)
-abs_error=np.zeros(100)
+error_x_r141=abs(full_search_result_x_r141-(groundtruth_r141[:,1]))
+error_y_r141=abs(full_search_result_y_r141-(groundtruth_r141[:,0]))
 
-for i in range(len(groundtruth)):
-    abs_error[i]=np.linalg.norm(sub_of_cal_ground[:,i])
+error_x_r2=abs(full_search_result_x_r2-(groundtruth_r2[:,1]))
+error_y_r2=abs(full_search_result_y_r2-(groundtruth_r2[:,0]))
 
-fig, (ax1,ax2,ax3)=plt.subplots(1,3,sharey=True,constrained_layout=False)
+error_x_r283=abs(full_search_result_x_r283-(groundtruth_r283[:,1]))
+error_y_r283=abs(full_search_result_y_r283-(groundtruth_r283[:,0]))
 
-fig.suptitle('Spot-finding for Perfect Spots of Radius 1 out of 100 Images was Done in, {}s'.format(format(timetaken,".2")))
+error_x_r4=abs(full_search_result_x_r4-(groundtruth_r4[:,1]))
+error_y_r4=abs(full_search_result_y_r4-(groundtruth_r4[:,0]))
+
+error_x_r566=abs(full_search_result_x_r566-(groundtruth_r566[:,1]))
+error_y_r566=abs(full_search_result_y_r566-(groundtruth_r566[:,0]))
+
+error_x_r8=abs(full_search_result_x_r8-(groundtruth_r8[:,1]))
+error_y_r8=abs(full_search_result_y_r8-(groundtruth_r8[:,0]))
+
+
+
+calculated_points_r1=np.stack((full_search_result_x_r1,full_search_result_y_r1),axis=1)
+sub_of_cal_ground_r1=groundtruth_r1.reshape(2,100)-calculated_points_r1.reshape(2,100)
+abs_error_r1=np.zeros(100)
+
+calculated_points_r141=np.stack((full_search_result_x_r141,full_search_result_y_r141),axis=1)
+sub_of_cal_ground_r141=groundtruth_r141.reshape(2,100)-calculated_points_r141.reshape(2,100)
+abs_error_r141=np.zeros(100)
+
+calculated_points_r2=np.stack((full_search_result_x_r2,full_search_result_y_r2),axis=1)
+sub_of_cal_ground_r2=groundtruth_r2.reshape(2,100)-calculated_points_r2.reshape(2,100)
+abs_error_r2=np.zeros(100)
+
+calculated_points_r283=np.stack((full_search_result_x_r283,full_search_result_y_r283),axis=1)
+sub_of_cal_ground_r283=groundtruth_r283.reshape(2,100)-calculated_points_r283.reshape(2,100)
+abs_error_r283=np.zeros(100)
+
+calculated_points_r4=np.stack((full_search_result_x_r4,full_search_result_y_r4),axis=1)
+sub_of_cal_ground_r4=groundtruth_r4.reshape(2,100)-calculated_points_r4.reshape(2,100)
+abs_error_r4=np.zeros(100)
+
+calculated_points_r566=np.stack((full_search_result_x_r566,full_search_result_y_r566),axis=1)
+sub_of_cal_ground_r566=groundtruth_r566.reshape(2,100)-calculated_points_r566.reshape(2,100)
+abs_error_r566=np.zeros(100)
+
+calculated_points_r8=np.stack((full_search_result_x_r8,full_search_result_y_r8),axis=1)
+sub_of_cal_ground_r8=groundtruth_r8.reshape(2,100)-calculated_points_r8.reshape(2,100)
+abs_error_r8=np.zeros(100)
+
+
+
+for i in range(len(groundtruth_r1)):
+    abs_error_r1[i]=np.linalg.norm(sub_of_cal_ground_r1[:,i])
+    abs_error_r141[i]=np.linalg.norm(sub_of_cal_ground_r141[:,i])
+    abs_error_r2[i]=np.linalg.norm(sub_of_cal_ground_r2[:,i])
+    abs_error_r283[i]=np.linalg.norm(sub_of_cal_ground_r283[:,i])
+    abs_error_r4[i]=np.linalg.norm(sub_of_cal_ground_r4[:,i])
+    abs_error_r566[i]=np.linalg.norm(sub_of_cal_ground_r566[:,i])
+    abs_error_r8[i]=np.linalg.norm(sub_of_cal_ground_r8[:,i])
+
 
 x=np.linspace(0,100,100)
 
-ax1.set_title('X-axis Error From X Part of Groundtruth',pad=10)
-ax1.scatter(x,error_x,marker='x')
-ax1.set_xlabel("Index of 100 Images")
-ax1.set_ylabel("Error")
-avg_x=np.average(error_x)
-med_x=np.median(error_x)
-ax1.axhline(avg_x,0,100,color='r',linestyle=':',label='Average {}'.format(format(avg_x,".2")))
-ax1.axhline(med_x,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_x,".2")))
-ax1.legend()
+fig, ((ax1,ax2,ax3,ax4),(ax5,ax6,ax7,ax8))=plt.subplots(2,4,sharey=True,sharex=False,tight_layout=True)
 
-ax2.set_title('Y-axis Error From Y Part of Groundtruth')
-ax2.scatter(x,error_y,marker='x')
-ax2.set_xlabel("Index of 100 Images")
-ax2.set_ylabel("Error")
-avg_y=np.average(error_y)
-med_y=np.median(error_y)
-ax2.axhline(avg_y,0,100,color='r',linestyle=':',label='Average {}'.format(format(avg_y,".2")))
-ax2.axhline(med_y,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_y,".2")))
-ax2.legend()
+print(format(timetaken,".2"))
 
 
-ax3.set_title('Euclidean Distance From Groundtruth')
-ax3.scatter(x,abs_error,marker='x')
-ax3.set_xlabel("Index of 100 Images")
-ax3.set_ylabel("Error")
-avg_abs=np.average(abs_error)
-med_abs=np.median(abs_error)
-ax3.axhline(avg_abs,0,100,color='r',linestyle=':',label='Average {}'.format(format(avg_abs,".2")))
-ax3.axhline(med_abs,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_abs,".2")))
-ax3.legend()
 
+fig.supxlabel('Index Of Images')
+fig.supylabel('Absolute Error (pixels)')
+
+ax1.set_title('R1.00')
+ax1.scatter(x,abs_error_r1,marker='x')
+avg_abs=np.average(abs_error_r1)
+med_abs=np.median(abs_error_r1)
+#ax1.hist(abs_error_r1,30)
+#ax1.vlines(avg_abs,0,8,color='r',label='Average')
+ax1.axhline(avg_abs,100,color='r',linestyle=':',label='Average')
+ax1.axhline(med_abs,0,100,color='k',linestyle=':',label='Median')
+
+ax2.set_title('R1.41')
+ax2.scatter(x,abs_error_r141,marker='x')
+avg_abs=np.average(abs_error_r141)
+med_abs=np.median(abs_error_r141)
+#ax2.hist(abs_error_r141,30)
+#ax2.vlines(avg_abs,0,8,color='r')
+ax2.axhline(avg_abs,0,100,color='r',linestyle=':')
+ax2.axhline(med_abs,0,100,color='k',linestyle=':')
+
+ax3.set_title('R2.00')
+ax3.scatter(x,abs_error_r2,marker='x')
+avg_abs=np.average(abs_error_r2)
+med_abs=np.median(abs_error_r2)
+#ax3.hist(abs_error_r2,30)
+#ax3.vlines(avg_abs,0,8,color='r')
+ax3.axhline(avg_abs,0,100,color='r',linestyle=':')
+ax3.axhline(med_abs,0,100,color='k',linestyle=':')
+
+ax4.set_title('R2.38')
+ax4.scatter(x,abs_error_r283,marker='x')
+aavg_abs=np.average(abs_error_r283)
+med_abs=np.median(abs_error_r283)
+#ax4.hist(abs_error_r283,30)
+#ax4.vlines(avg_abs,0,8,color='r')
+ax4.axhline(avg_abs,0,100,color='r',linestyle=':')
+ax4.axhline(med_abs,0,100,color='k',linestyle=':')
+
+ax5.set_title('R4.00')
+ax5.scatter(x,abs_error_r4,marker='x')
+avg_abs=np.average(abs_error_r4)
+med_abs=np.median(abs_error_r4)
+#ax5.hist(abs_error_r4,30)
+#ax5.vlines(avg_abs,0,8,color='r')
+ax5.axhline(avg_abs,0,100,color='r',linestyle=':')
+ax5.axhline(med_abs,0,100,color='k',linestyle=':')
+
+ax6.set_title('R5.66')
+ax6.scatter(x,abs_error_r566,marker='x')
+avg_abs=np.average(abs_error_r566)
+med_abs=np.median(abs_error_r566)
+#ax6.hist(abs_error_r566,30)
+#ax6.vlines(avg_abs,0,8,color='r')
+ax6.axhline(avg_abs,0,100,color='r',linestyle=':')
+ax6.axhline(med_abs,0,100,color='k',linestyle=':')
+
+ax7.set_title('R8.00')
+ax7.scatter(x,abs_error_r8,marker='x')
+avg_abs=np.average(abs_error_r8)
+med_abs=np.median(abs_error_r8)
+#ax7.hist(abs_error_r8,30)
+#ax7.vlines(avg_abs,0,8,color='r')
+ax7.axhline(avg_abs,0,100,color='r',linestyle=':')
+ax7.axhline(med_abs,0,100,color='k',linestyle=':')
+plt.delaxes(ax8)
+fig.legend(loc='lower right')
+plt.tight_layout()
 plt.show()
 ## maybe put a min and max hline on the graphs
 ###
 #All for display#
 ###
 
+'''
+### extra that i may or may not need
 
-#guess=search3(file_sum,0)
-#print(guess,groundtruth[0,1]+25)
-#
-#tri_var=triangle(24.1,3,1.6e5)
-#area_var=area(tri_var,file_sum)
-#plt.plot(area_var)
-#plt.plot(file_sum[0,:])
-#plt.show()
+fig, (ax1,ax2,ax3)=plt.subplots(1,3,sharey=True,sharex=False,tight_layout=True)
+
+ax1.set_title('A',pad=10)
+ax1.scatter(x,error_x_r1,marker='x')
+avg_x=np.average(error_x_r1)
+med_x=np.median(error_x_r1)
+ax1.axhline(avg_x,0,100,color='r',linestyle=':',label='Average {}'.format(format(avg_x,".2")))
+ax1.axhline(med_x,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_x,".2")))
+ax1.legend()
+
+ax2.set_title('B')
+ax2.scatter(x,error_y_r1,marker='x')
+avg_y=np.average(error_y_r1)
+med_y=np.median(error_y_r1)
+ax2.axhline(avg_y,0,100,color='r',linestyle=':',label='Average {}'.format(format(avg_y,".2")))
+ax2.axhline(med_y,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_y,".2")))
+ax2.legend()
+
+ax3.set_title('C')
+ax3.scatter(x,abs_error_r1,marker='x')
+avg_abs=np.average(abs_error_r1)
+med_abs=np.median(abs_error_r1)
+#ax1.hist(abs_error_r1,30)
+#ax1.vlines(avg_abs,0,8,color='r',label='Average')
+ax3.axhline(avg_abs,100,color='r',linestyle=':',label='Average {}'.format(format(avg_abs,".2")))
+ax3.axhline(med_abs,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_abs,".2")))
+ax3.legend()
+
+fig.supxlabel('Index of 100 Images')
+fig.supylabel('Error (pixels)')
+plt.savefig('single_test.png',dpi=400)
+'''
+'''
+fig, (ax1,ax2)=plt.subplots(1,2,sharey=True,sharex=False,tight_layout=True)
+
+ax1.set_title('A')
+ax1.scatter(x,abs_error_r1,marker='x')
+avg_abs=np.average(abs_error_r1)
+med_abs=np.median(abs_error_r1)
+#ax1.hist(abs_error_r1,30)
+#ax1.vlines(avg_abs,0,8,color='r',label='Average')
+ax1.axhline(avg_abs,100,color='r',linestyle=':',label='Average {}'.format(format(avg_abs,".2")))
+ax1.axhline(med_abs,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_abs,".2")))
+ax1.set_xlabel('Image Stack')
+ax1.legend()
+
+ax2.set_title('B')
+ax2.hist(abs_error_r1,bins=30,orientation='horizontal')
+ax2.axhline(avg_abs,100,color='r',linestyle=':',label='Average {}'.format(format(avg_abs,".2")))
+ax2.axhline(med_abs,0,100,color='k',linestyle=':',label='Median {}'.format(format(med_abs,".2")))
+ax2.set_xlabel('Count')
+
+fig.supylabel('Absolute Error (pixels)')
+plt.savefig('single_histo.png',dpi=400)
+'''
